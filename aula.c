3c57016dc50a37ab1a0ce62fbf9b85195d586e38
André Rocha <andrenakarocha@hotmail.com>
2024-04-05 10:48:45 -0300

    Aula 03 Adicionada

---
+++ aula.c
@@ -0,0 +1,17 @@
+/* Código para printar texto ao apertar um botão
+//Definir Botão
+void setup()
+{
+  pinMode(2, INPUT); //Definir porta
+  Serial.begin(9600); //Mandar informações
+}
+ 
+//Loop
+void loop()
+{
+  if (digitalRead(2) == HIGH) //Quando botão pressionado
+  {
+     Serial.println("Botao pressionado!"); //Print
+  }
+}
+*/
